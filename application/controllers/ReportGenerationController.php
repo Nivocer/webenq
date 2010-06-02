1e6632db16f036ee95572a68a3511fc7a61d7b85
bart <bart>
2010-06-02 15:47:25 +0000

    fix: rapport open antwoorden aangepast, zodat alle vragen getoond worden (vragen moesten in sectie "detail" staan om herhaald te worden, ipv in "summary" - zie ook het hoofdstuk over sections in definitive jasperreports guide)

--- application/controllers/ReportGenerationController.php
+++ application/controllers/ReportGenerationController.php
@@ -24,14 +24,22 @@ class ReportGenerationController extends Zend_Controller_Action
     	$user	= $config->resources->db->params->username;
     	$pass	= $config->resources->db->params->password;
     	
+    	/* remove old report */
+    	$repDef = new HVA_Model_DbTable_ReportDefinitions();
+    	$row = $repDef->find($this->_id)->current();
+    	$file = APPLICATION_PATH . "/../public/reports/" . $row->output_filename . "." . $row->output_format;
+    	if (file_exists($file)) {
+    		unlink($file);
+    	}
+    	
+    	/* create new report */
     	$cwd = getcwd();
     	chdir(APPLICATION_PATH . "/../java");
     	$cmd = "java -cp .:./lib/bisiLibJasper.jar:./lib/bisiResources.jar:./lib/mysql-connector-java-5.1.6-bin.jar:./lib/poi-3.5-FINAL-20090928.jar:./lib/jasperreports-3.7.2.jar:./lib/iText-2.1.7.jar:./lib/commons-logging-1.1.1.jar:./lib/commons-digester-2.0.jar:./lib/commons-collections-3.2.1.jar:./lib/commons-beanutils-1.8.3.jar it.bisi.report.jasper.ExecuteReport $host/$db $user $pass $this->_id";
     	exec($cmd);
     	chdir($cwd);
     	
-    	$repDef = new HVA_Model_DbTable_ReportDefinitions();
-    	$row = $repDef->find($this->_id)->current();
+    	/* assign filename to view */    	
     	$this->view->file = $row->output_filename . "." . $row->output_format;
     }
 }
