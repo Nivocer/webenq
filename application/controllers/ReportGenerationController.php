a544e12c996570feb76a9fe12c9bdf7226106419
bart <bart>
2010-06-10 12:55:35 +0000

    add: verbinding db loopt nu via 127.0.0.1:6603 add: nieuw wachtwoord

--- application/controllers/ReportGenerationController.php
+++ application/controllers/ReportGenerationController.php
@@ -20,6 +20,7 @@ class ReportGenerationController extends Zend_Controller_Action
     {
     	$config = new Zend_Config_Ini(APPLICATION_PATH . "/configs/application.ini", APPLICATION_ENV);
     	$host	= $config->resources->db->params->host;
+    	$port	= $config->resources->db->params->port;
     	$db		= $config->resources->db->params->dbname;
     	$user	= $config->resources->db->params->username;
     	$pass	= $config->resources->db->params->password;
@@ -35,7 +36,7 @@ class ReportGenerationController extends Zend_Controller_Action
     	/* create new report */
     	$cwd = getcwd();
     	chdir(APPLICATION_PATH . "/../java");
-    	$cmd = "java -cp .:./lib/bisiLibJasper.jar:./lib/bisiResources.jar:./lib/mysql-connector-java-5.1.6-bin.jar:./lib/poi-3.5-FINAL-20090928.jar:./lib/jasperreports-3.7.2.jar:./lib/iText-2.1.7.jar:./lib/commons-logging-1.1.1.jar:./lib/commons-digester-2.0.jar:./lib/commons-collections-3.2.1.jar:./lib/commons-beanutils-1.8.3.jar it.bisi.report.jasper.ExecuteReport $host/$db $user $pass $this->_id";
+    	$cmd = "java -cp .:./lib/bisiLibJasper.jar:./lib/bisiResources.jar:./lib/mysql-connector-java-5.1.6-bin.jar:./lib/poi-3.5-FINAL-20090928.jar:./lib/jasperreports-3.7.2.jar:./lib/iText-2.1.7.jar:./lib/commons-logging-1.1.1.jar:./lib/commons-digester-2.0.jar:./lib/commons-collections-3.2.1.jar:./lib/commons-beanutils-1.8.3.jar it.bisi.report.jasper.ExecuteReport $host:$port/$db $user $pass $this->_id";
     	exec($cmd);
     	chdir($cwd);
     	
