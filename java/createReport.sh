b05cc27c4622563b09b44787f88a52a4e2cf7317
jaapandre <jaapandre>
2010-07-14 14:27:58 +0000

    mogelijk om snel lokaal/server aan te sturen

--- java/createReport.sh
+++ java/createReport.sh
@@ -4,6 +4,9 @@
 echo ------ >> createReport.error
 echo $1 >> createReport.error
 echo ------ >> createReport.error
+#lokaal
+#java -cp .:./lib/bisiLibJasper.jar:./lib/bisiResources.jar:./lib/mysql-connector-java-5.1.6-bin.jar:./lib/poi-3.5-FINAL-20090928.jar:./lib/jasperreports-3.7.2.jar:./lib/iText-2.1.7.jar:./lib/commons-logging-1.1.1.jar:./lib/commons-digester-2.0.jar:./lib/commons-collections-3.2.1.jar:./lib/commons-beanutils-1.8.3.jar it.bisi.report.jasper.ExecuteReport 127.0.0.1/webenq_org_hva hva  6V080HEp0hFzAGOv $1 2>>createReport.error
+#server
 java -cp .:./lib/bisiLibJasper.jar:./lib/bisiResources.jar:./lib/mysql-connector-java-5.1.6-bin.jar:./lib/poi-3.5-FINAL-20090928.jar:./lib/jasperreports-3.7.2.jar:./lib/iText-2.1.7.jar:./lib/commons-logging-1.1.1.jar:./lib/commons-digester-2.0.jar:./lib/commons-collections-3.2.1.jar:./lib/commons-beanutils-1.8.3.jar it.bisi.report.jasper.ExecuteReport 127.0.0.1:6603/webenq_org_hva hva  6V080HEp0hFzAGOv $1 2>>createReport.error
 #more createReport.error
 
