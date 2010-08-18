fe88e5bfee23dbac25658c66e1477cba025bad36
bart <bart>
2010-08-18 13:52:22 +0000

    [0004350] correctie bij herkenning numerike waarden (null-waarden negeren), enkele tests uitgeschakeld (zie notes in issue 4350), testdata aangepast, opmaak output unittest-resultaten

--- tests/index.php
+++ tests/index.php
@@ -1,12 +1,23 @@
 <?php
 	ob_start();
 	system('phpunit');
-//	system('phpunit --testdox');
 	$output = ob_get_clean();
+	$output = nl2br($output);
 ?>
-
-<a href="log/coverage">coverage log</a>
-
-<pre>
-	<?php echo htmlspecialchars($output); ?>	
-</pre>
+<html>
+	<head>
+		<style type="text/css">
+			body {
+				font: verdana 10px;
+			}
+		</style>
+	</head>
+	<body>
+		<p>
+			<a href="log/coverage">coverage log</a>
+		</p>
+		<div>
+			<?php echo $output; ?>
+		</div>
+	</body>
+</html>
