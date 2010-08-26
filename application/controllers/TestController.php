d0adfc36db6e852a7d4f184fc3f8a48fad3cb53d
bart <bart>
2010-08-26 08:57:04 +0000

    [4350] snelle tester toegevoegd, hiermee kan een van de bestanden met testdata gebruikt worden ipv het (langdurende) hele unittesting-proces doorlopen

---
+++ application/controllers/TestController.php
@@ -0,0 +1,61 @@
+<?php
+
+class TestController extends Zend_Controller_Action
+{
+	/**
+	 * Name of the file to process
+	 */
+	protected $_filename;
+	
+    /**
+     * Index action
+     */
+    public function indexAction()
+    {
+    	$form = new HVA_Form_Test_Index();
+    	$errors = array();
+    	$question = null;
+    	
+    	if ($this->getRequest()->isPost()) {
+    		if ($form->isValid($this->getRequest()->getPost())) {
+    			
+    			$form->file->receive();
+    			
+    			if (!$form->file->isReceived()) {
+    				$errors[] = 'Error receiving the file';
+    			}
+    			
+    			if (!$errors) {
+    				try {
+    					$filename = $form->file->getFileName();
+    					$question = $this->_processTest($filename);
+    				} catch (Exception $e) {
+    					$errors[] = 'Error processing the file: ' . $e->getMessage();
+    				}
+    			}    			
+    		}
+    	}
+    	
+    	$this->view->form = $form;
+    	$this->view->errors = $errors;    	
+    	$this->view->question = $question;    	
+    }
+    
+    
+    /**
+     * Processes the testfile
+     * 
+     * @return void
+     */
+    protected function _processTest($file)
+    {
+    	$f = fopen($file, 'r');
+    	
+    	$data = array();
+    	while ($line = fgets($f)) {
+    		$data[] = $line;
+    	}
+    	
+    	return HVA_Model_Data_Question::factory($data);
+    }
+}
