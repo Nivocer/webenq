db77247bb071d68d022ce0eb819b617abb5408b4
jaapandre <jaapandre>
2010-09-16 12:51:14 +0000

    betere aansluiting bij mass import, hoogste en laagste aangeven

--- scripts/interpratie.sh
+++ scripts/interpratie.sh
@@ -1,27 +1,16 @@
-wget "http://hva.localhost/interpretation/index/id/19"
-wget "http://hva.localhost/interpretation/index/id/20"
-wget "http://hva.localhost/interpretation/index/id/21"
-#wget "http://hva.localhost/interpretation/index/id/22"
-wget "http://hva.localhost/interpretation/index/id/23"
-wget "http://hva.localhost/interpretation/index/id/24"
-wget "http://hva.localhost/interpretation/index/id/25"
-wget "http://hva.localhost/interpretation/index/id/26"
-wget "http://hva.localhost/interpretation/index/id/27"
-wget "http://hva.localhost/interpretation/index/id/28"
-#wget "http://hva.localhost/interpretation/index/id/29"
-wget "http://hva.localhost/interpretation/index/id/30"
-wget "http://hva.localhost/interpretation/index/id/31"
-wget "http://hva.localhost/interpretation/index/id/32"
-wget "http://hva.localhost/interpretation/index/id/33"
-wget "http://hva.localhost/interpretation/index/id/34"
-wget "http://hva.localhost/interpretation/index/id/35"
-wget "http://hva.localhost/interpretation/index/id/36"
-wget "http://hva.localhost/interpretation/index/id/37"
-wget "http://hva.localhost/interpretation/index/id/38"
-wget "http://hva.localhost/interpretation/index/id/39"
-wget "http://hva.localhost/interpretation/index/id/40"
-wget "http://hva.localhost/interpretation/index/id/41"
-wget "http://hva.localhost/interpretation/index/id/42"
-#wget "http://hva.localhost/interpretation/index/id/43"
-wget "http://hva.localhost/interpretation/index/id/44"
+#!/bin/sh
+start=90
+end=93
+
+start=98
+end=113
+
+x=$start
+while [ $x -le $end ]
+do
+echo $x
+wget "http://hva-devel.localhost/interpretation/index/id/$x"
+x=$(($x+1))
+done
+
 
