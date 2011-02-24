e781f96a44b8d5b7381d8dc2592d4d1b830b2708
bart <bart>
2011-02-24 14:15:49 +0000

    fixed [4110] null-values uit code gehaald en naar db

---
+++ application/forms/AnswerPossibilityNullValue/Add.php
@@ -0,0 +1,27 @@
+<?php
+class HVA_Form_AnswerPossibilityNullValue_Add extends Zend_Form
+{
+	/**
+	 * Builds the form
+	 * 
+	 * @return void
+	 */
+	public function init()
+	{
+		$this->addElements(array(
+			$this->createElement('text', 'value', array(
+				'label' => 'Naam:',
+				'required' => true,
+				'validators' => array(
+					new Zend_Validate_NotEmpty(),
+				),
+				'filters' => array(
+					new Zend_Filter_StringToLower(),
+				),
+			)),
+			$this->createElement('submit', 'submit', array(
+				'label' => 'opslaan',
+			)),
+		));
+	}
+}
---
+++ application/forms/AnswerPossibilityNullValue/Edit.php
@@ -0,0 +1,43 @@
+<?php
+class HVA_Form_AnswerPossibilityNullValue_Edit extends HVA_Form_AnswerPossibilityNullValue_Add
+{
+	/**
+	 * Current answer-possibility-null-value
+	 * 
+	 * @var AnswerPossibilityNullValue $_answerPossibilityNullValue
+	 */
+	protected $_answerPossibilityNullValue;
+	
+	/**
+	 * Class constructor
+	 * 
+	 * @param AnswerPossibilityNullValue $answerPossibilityNullValue
+	 * @param array|Zend_Config $options
+	 * @return void
+	 */
+	public function __construct(AnswerPossibilityNullValue $answerPossibilityNullValue, array $options = null)
+	{
+		$this->_answerPossibilityNullValue = $answerPossibilityNullValue;
+		parent::__construct($options);
+	}
+	
+	/**
+	 * Builds the form
+	 * 
+	 * @return void
+	 */
+	public function init()
+	{
+		parent::init();
+		
+		$this->setAttrib('autocomplete', 'off');
+		
+		$this->getElement('value')->setValue($this->_answerPossibilityNullValue->value);
+		
+		$this->addElements(array(
+			$this->createElement('hidden', 'id', array(
+				'value' => $this->_answerPossibilityNullValue->id,
+			)),
+		));
+	}
+}
