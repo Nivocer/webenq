7c63bbb05a507abd720b59469433537dee46a7d1
bart <bart>
2011-06-09 13:07:12 +0000

    [4825] vraag alleen uit huidige questionnaire verwijderen

--- application/forms/QuestionnaireQuestion/Delete.php
+++ application/forms/QuestionnaireQuestion/Delete.php
@@ -3,14 +3,14 @@ class Webenq_Form_QuestionnaireQuestion_Delete extends Zend_Form
 {
     /**
      * QuestionnaireQuestion instance
-     * 
+     *
      * @var QuestionnaireQuestion $_questionnaireQuestion
      */
     protected $_questionnaireQuestion;
-    
+
     /**
      * Constructor
-     * 
+     *
      * @param QuestionnaireQuestion $questionnaireQuestion
      * @param mixed $options
      */
@@ -19,28 +19,20 @@ class Webenq_Form_QuestionnaireQuestion_Delete extends Zend_Form
         $this->_questionnaireQuestion = $questionnaireQuestion;
         parent::__construct($options);
     }
-    
+
     /**
      * Initialises the form
-     * 
+     *
      * @return void
      */
     public function init()
     {
         $qq = $this->_questionnaireQuestion;
-        
+
         $this->addElements(array(
             $this->createElement('hidden', 'questionnaire_question_id', array(
                 'decorators' => array('ViewHelper'),
                 'value' => $this->_questionnaireQuestion->id,
-            )),
-            $this->createElement('radio', 'change_globally', array(
-                'label' => 'Hoe wilt u bovenstaande vraag verwijderen?',
-                'value' => 'local',
-                'multiOptions' => array(
-                    'local' => 'Uit huidige questionnaire verwijderen',
-                    'global' => 'Volledig verwijderen',
-                ),
             )),
             $this->createElement('submit', 'yes', array(
                 'label' => 'ja',
@@ -51,6 +43,6 @@ class Webenq_Form_QuestionnaireQuestion_Delete extends Zend_Form
                 'decorators' => array('ViewHelper'),
             )),
         ));
-        
+
     }
 }
