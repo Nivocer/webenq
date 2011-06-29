6075975c2e6549f0eaa467783423004b1f1ef087
bart <bart>
2011-06-29 09:36:50 +0000

    [4827] tests added for form validation

---
+++ tests/application/forms/Question/AddTest.php
@@ -0,0 +1,34 @@
+<?php
+class Webenq_Test_Form_Question_AddTest extends Webenq_Test_Form
+{
+    protected $_form;
+
+    public function setUp()
+    {
+        $this->_form = new Webenq_Form_Question_Add();
+        parent::setUp();
+    }
+
+    public function testOneLanguageIsRequired()
+    {
+        $form = $this->_form;
+
+        // invalid without languages
+        $values = array('text' => array(
+            'en' => '',
+            'nl' => ''));
+        $this->assertFalse($form->isValid($values));
+
+        // valid with one language
+        $values = array('text' => array(
+            'en' => 'test',
+            'nl' => ''));
+        $this->assertTrue($form->isValid($values));
+
+        // valid with all languages
+        $values = array('text' => array(
+            'en' => 'test',
+            'nl' => 'test'));
+        $this->assertTrue($form->isValid($values));
+    }
+}
