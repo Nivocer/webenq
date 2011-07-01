45e376f2a82a3d3a350f2ca5ab2c960a46aeb3b4
bart <bart>
2011-07-01 08:03:23 +0000

    [5170] unquote magic quotes

--- application/plugins/Request.php
+++ application/plugins/Request.php
@@ -1,12 +1,29 @@
 <?php
 class Webenq_Plugin_Request extends Zend_Controller_Plugin_Abstract
 {
-    public function preDispatch(Zend_Controller_Request_Abstract $request)
+    public function dispatchLoopStartup(Zend_Controller_Request_Abstract $request)
+    {
+        $this->_setLayout($request);
+        $this->_unQuotePost($request);
+    }
+
+    protected function _setLayout($request)
     {
         if ($request->isXmlHttpRequest()) {
             if (!$request->getParam('format')) {
                 $request->setParam('format', 'html');
             }
+        }
+    }
+
+    protected function _unQuotePost($request)
+    {
+        if ($request->isPost() && (bool) ini_get('magic_quotes_gpc')) {
+            $unQuoted = array();
+            foreach ($request->getPost() as $key => $val) {
+                $unQuoted[stripslashes($key)] = stripslashes($val);
+            }
+            $request->setPost($unQuoted);
         }
     }
 }
