4353ca2666c5fb4aa0ca68c21da9d8683a2352b6
bart <bart>
2011-07-05 11:44:48 +0000

    dummy test verwijderd

--- tests/application/controllers/IndexControllerTest.php
+++ tests/application/controllers/IndexControllerTest.php
@@ -1,12 +1,9 @@
 <?php
 class Webenq_Test_ControllerTestCase_IndexControllerTest extends Webenq_Test_Controller
 {
-    public function test() {}
-
-//	public function testIndexActionRendersLoginForm()
-//    {
+	public function testIndexActionRendersLoginForm()
+    {
 //       	$this->dispatch('/');
-//        $this->assertQuery('input#username');
-//        $this->assertQuery('input#password');
-//    }
+//       	$this->assertRedirectTo('/user/login');
+    }
 }
