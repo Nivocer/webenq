18d8674252c6558808ae9fb4011a43cf2b1ec765
bart <bart>
2011-07-05 11:51:08 +0000

    Test toegevoegd: wordt een form-element aangemaakt?

---
+++ tests/application/models/QuestionnaireQuestionTest.php
@@ -0,0 +1,10 @@
+<?php
+class Webenq_Test_Model_QuestionnaireQuestionTest extends Webenq_Test_Model
+{
+    public function testFormElementIsGenerated()
+    {
+        $qq = new Webenq_Model_QuestionnaireQuestion();
+        $elm = $qq->getFormElement();
+        $this->assertTrue($elm instanceof Zend_Form_Element);
+    }
+}
