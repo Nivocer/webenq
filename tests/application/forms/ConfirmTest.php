e86a918f1b85430f956bd1bdde7cc3973b19df0b
bart <bart>
2011-07-05 11:47:43 +0000

    Test toegevoegd om te controleren of id en melding in confirmation-form komen

---
+++ tests/application/forms/ConfirmTest.php
@@ -0,0 +1,19 @@
+<?php
+class Webenq_Test_Form_ConfirmTest extends Webenq_Test_Form
+{
+    protected $_id = 123;
+
+    protected $_text = 'Weet u het zeker?';
+
+    public function setUp()
+    {
+        $this->_form = new Webenq_Form_Confirm($this->_id, $this->_text);
+        parent::setUp();
+    }
+
+    public function testIdAndTextAreSet()
+    {
+        $this->assertTrue($this->_form->id->getValue() == $this->_id);
+        $this->assertTrue($this->_form->id->getLabel() == $this->_text);
+    }
+}
