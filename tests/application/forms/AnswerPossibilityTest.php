b817acc6a326fe183c73e7c73f1996fab9401642
bart <bart>
2011-07-05 11:48:24 +0000

    Onnodige class var verwijderd

--- tests/application/forms/AnswerPossibilityTest.php
+++ tests/application/forms/AnswerPossibilityTest.php
@@ -1,16 +1,14 @@
 <?php
 class Webenq_Test_Form_AnswerPossibilityTest extends Webenq_Test_Form
 {
-    protected $_form;
-
     public function setUp()
     {
-        parent::setUp();
-
         $answerPossibilityGroup = new AnswerPossibilityGroup();
         $answerPossibilityGroup->id = 1;
         $language = 'nl';
         $this->_form = new Webenq_Form_AnswerPossibility_Add($answerPossibilityGroup, $language);
+
+        parent::setUp();
     }
 
     public function testTextIsRequired()
