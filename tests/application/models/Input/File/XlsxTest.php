fd89c33031bfbb5adc8887519bd239cb6a1868c2
bart <bart>
2011-07-05 11:49:49 +0000

    Webenq_Model_Input_File tests geherstructureerd

--- tests/application/models/Input/File/XlsxTest.php
+++ tests/application/models/Input/File/XlsxTest.php
@@ -2,25 +2,7 @@
 
 class Webenq_Test_Model_Input_File_XlsxTest extends Webenq_Test_Model_Input_FileTest
 {
-//    /**
-//     * @expectedException Exception
-//     */
-//    public function testInstantiateClassWithInvalidFileThrowsException()
-//    {
-//    	Webenq_Import_Adapter_Abstract::factory('./thisFileDoesNotExist.xlsx');
-//    }
-//
-//    public function testFileHasBeenOpenedForReading()
-//    {
-//    	$adapter = Webenq_Import_Adapter_Abstract::factory('testdata/testdata.xlsx');
-//    	$this->assertTrue(is_object($adapter));
-//    }
-//
-//    public function testDataHasBeenReadFromFile()
-//    {
-//    	$adapter = Webenq_Import_Adapter_Abstract::factory('testdata/testdata.xlsx');
-//    	$data = $adapter->getData();
-//    	$this->assertTrue(is_array($data));
-//    	$this->assertTrue(count($data) > 0);
-//    }
+    protected $_validFile = 'testdata/testdata.xlsx';
+
+    protected $_invalidFile = 'thisFileDoesNotExist.xlsx';
 }
