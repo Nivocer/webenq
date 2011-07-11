12db7e37981cceda8aa7fc2c1d7d38a7ca0b9420
bart <bart>
2011-07-11 14:54:47 +0000

    [5196] application.ini bevat nu alleen nog applicatie-specifieke settings, de rest kan in override.ini (welk niet in cvs mag)

--- public/index.php
+++ public/index.php
@@ -1,32 +1,13 @@
 <?php
 
-/* Set time zone */
-if (function_exists('date_default_timezone_set')) {
-	date_default_timezone_set('Europe/Amsterdam');
-}
-
-/* Define path to application directory */
-defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
-
-/* Define application environment */
+// define application environment (if not set yet)
 defined('APPLICATION_ENV')
     || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
 
-/* Ensure library/ is on include_path */
-set_include_path(implode(PATH_SEPARATOR, array(
-	realpath(APPLICATION_PATH . '/../libraries'),
-	realpath(APPLICATION_PATH . '/../classes'),
-    get_include_path(),
-)));
+// initialize
+require_once realpath(dirname(__FILE__) . '/init.php');
 
-/* Zend_Application */
+// create application, bootstrap, and run
 require_once 'Zend/Application.php';
-
-/* Create application, bootstrap, and run */
-$application = new Zend_Application(
-    APPLICATION_ENV,
-    APPLICATION_PATH . '/configs/application.ini'
-);
-
+$application = new Zend_Application(APPLICATION_ENV, $config->{APPLICATION_ENV});
 $application->bootstrap()->run();
---
+++ public/init.php
@@ -0,0 +1,28 @@
+<?php
+
+// set default time zone
+if (function_exists('date_default_timezone_set')) {
+	date_default_timezone_set('Europe/Amsterdam');
+}
+
+// define path to application directory (if not set yet)
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+
+// set include paths
+set_include_path(implode(PATH_SEPARATOR, array(
+	realpath(APPLICATION_PATH . '/../libraries'),
+	realpath(APPLICATION_PATH . '/../classes'),
+    get_include_path(),
+)));
+
+// get configuration
+require_once 'Zend/Config/Ini.php';
+$config = new Zend_Config_Ini(APPLICATION_PATH . '/configs/application.ini', null, array('allowModifications' => true));
+if (file_exists(APPLICATION_PATH . '/configs/override.ini')) {
+    $override = new Zend_Config_Ini(APPLICATION_PATH . '/configs/override.ini');
+    $config->merge($override)->setReadOnly();
+}
+if (!$config->{APPLICATION_ENV}) {
+    throw new Exception('No configuration found for application environment "' . APPLICATION_ENV . '"');
+}
