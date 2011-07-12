3b83a1a87fa7d46f6760d5f8a7423fb08c258195
bart <bart>
2011-07-12 13:15:33 +0000

    fix: flash-messenger verwachtte een array, maar als er geen messages zijn krijgt hij null => dat leverde een application-error op

--- application/views/helpers/Messenger.php
+++ application/views/helpers/Messenger.php
@@ -2,14 +2,14 @@
 
 class Zend_View_Helper_Messenger extends Zend_View_Helper_Abstract
 {
-    public function messenger(array $messages)
+    public function messenger($messages = null)
     {
-        if (count($messages) > 0) {
+        if (is_array($messages) && count($messages) > 0) {
             $html = '';
             foreach ($messages as $message) {
                 $html .= "<span>$message</span>";
             }
-            
+
             return "<div id=\"flash_messenger\">$html</div>";
         }
     }
