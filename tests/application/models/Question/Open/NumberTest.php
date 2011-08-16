5150b9ceccc567db78da608b48b5d062b9e91042
bart <bart>
2011-08-16 11:23:26 +0000

    [5010] test toegevoegd voor interpretatie diverse formaten nummers met punten en komma's

---
+++ tests/application/models/Question/Open/NumberTest.php
@@ -0,0 +1,50 @@
+<?php
+class Webenq_Test_Model_Question_Open_NumberTest extends Webenq_Test_Model_Question_OpenTest
+{
+    /**
+     * @dataProvider provideValidData
+     */
+    public function testFactoryWithValidDataReturnsType($data)
+    {
+        $question = Webenq_Model_Question::factory($data);
+    	$this->assertTrue($question instanceof Webenq_Model_Question_Open_Number);
+    }
+
+    /**
+     * @dataProvider provideInvalidData
+     */
+    public function testFactoryWithInvalidDataDoesNotReturnType($data)
+    {
+        $question = Webenq_Model_Question::factory($data);
+    	$this->assertFalse($question instanceof Webenq_Model_Question_Open_Number);
+    }
+
+    public function testCalculatedSumOfValuesIsCorrect()
+    {
+        $data = array(1,1,1,1,2,2,2,3,3,4,4,4,5,5,5,5);
+    	$question = new Webenq_Model_Question_Open_Number();
+    	$question->setAnswerValues($data);
+
+    	$actualResult = $question->sum();
+    	$expectedResult = 48;
+    	$this->assertTrue($actualResult == $expectedResult);
+    }
+
+    public function testCalculatedSumOfStringsIsZero()
+    {
+        $data = array('test', 'test');
+    	$question = new Webenq_Model_Question_Open_Number();
+    	$question->setAnswerValues($data);
+
+    	$actualResult = $question->sum();
+    	$expectedResult = 0;
+    	$this->assertTrue($actualResult == $expectedResult);
+    }
+
+    public function testPeriodsAndCommasAreInterpretedCorrectly()
+    {
+        $validNumbers = array('1.0', '2,1', '2.100', '3,514.00', '+50', -50, 3.5, '-50.578,12');
+        $question = Webenq_Model_Question::factory($validNumbers);
+        $this->assertTrue($question instanceof Webenq_Model_Question_Open_Number);
+    }
+}
