c990b988f9a24376edc17202eb409ef0c6b34ad5
bart <bart>
2011-08-22 10:02:46 +0000

    doctrine fixtures, schema en migrations verplaats

--- tests/cases/Controller.php
+++ tests/cases/Controller.php
@@ -10,7 +10,7 @@ abstract class Webenq_Test_Case_Controller extends Zend_Test_PHPUnit_ControllerT
 
         $config = $application->getBootstrap()->getOption('doctrine');
         Doctrine_Core::createTablesFromModels($config['models_path']);
-        Doctrine_Core::loadData($config['data_fixtures_path'] . '/testing.yml');
+        Doctrine_Core::loadData($config['fixtures_path'] . '/testing.yml');
 
         $this->getFrontController()->setControllerDirectory(APPLICATION_PATH . '/controllers');
     }
