0ae3f5e6b5b9e1e30be7011759f66deb0dbc8641
bart <bart>
2011-11-01 09:31:17 +0000

    [5329] installation-script

--- bin/doctrine.php
+++ bin/doctrine.php
@@ -6,12 +6,13 @@ define('APPLICATION_ENV', 'development');
 
 // initialize
 require_once realpath(dirname(__FILE__) . '/../public/init.php');
+$application->bootstrap();
 
 $bootstrap = $application->getBootstrap();
 $config = $bootstrap->getOption('resources');
 $db = $config['db']['params'];
-$url = 'mysql://' . $db['username'] . ':' . $db['password'] . '@' . $db['host'] . ':' . $db['port'] . '/' .  $db['dbname'];
-$conn = Doctrine_Manager::connection($url, 'doctrine');
+$dsn = 'mysql://' . $db['username'] . ':' . $db['password'] . '@' . $db['host'] . ':' . $db['port'] . '/' .  $db['dbname'];
+Doctrine_Manager::connection($dsn, 'doctrine');
 
 $cli = new Doctrine_Cli($bootstrap->getOption('doctrine'));
 $cli->run($_SERVER['argv']);
