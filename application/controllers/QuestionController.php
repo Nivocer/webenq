5d28d86da0995d04c374e9d41b68f72d6c071358
bart <bart>
2011-11-24 14:40:07 +0000

    [5291] "Weet u zeker" in het Engels in formulier zodat het vertaad kan worden, en binnen t() zetten zodat het de vertaalfunctie ook daadwerkelijk bereikt

--- application/controllers/QuestionController.php
+++ application/controllers/QuestionController.php
@@ -144,8 +144,9 @@ class QuestionController extends Zend_Controller_Action
         $question = Doctrine_Core::getTable('Webenq_Model_Question')
             ->find($this->_request->id);
 
-        $confirmationText = 'Are you sure you want to delete the question "' . $question->QuestionText[0]->text .
-            '" (including all translations)?';
+        $confirmationText = t('Are you sure you want to delete the question "')
+            . $question->QuestionText[0]->text
+            . t('" (including all translations)?');
 
         $form = new Webenq_Form_Confirm($question->id, $confirmationText);
         $form->setAction($this->view->baseUrl('/question/delete/id/' . $this->_request->id));
