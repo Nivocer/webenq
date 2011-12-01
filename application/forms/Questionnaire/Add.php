47c10e6a5db7d947f622586038b75ce87b27b185
jaapandre <jaapandre>
2011-12-01 12:26:36 +0000

    fixed [5366] titel komt veilig in de database, en als het een bestandsnaam moet worden, dan wordt dat netjes gedaan, er is geen reden om hier een beperking op de titel te hebben

--- application/forms/Questionnaire/Add.php
+++ application/forms/Questionnaire/Add.php
@@ -10,15 +10,12 @@ class Webenq_Form_Questionnaire_Add extends Zend_Form
 {
     public function init()
     {
-        $regex = new Zend_Validate_Regex('/^[\w\d\s\?\.\+\-_,=\'"]*$/');
-        $regex->setMessage('The title contains invalid characters, valid characters are: a-z A-Z 1-9 _ - = + \' " . , ?', Zend_Validate_Regex::NOT_MATCH);
-
         foreach (Webenq_Language::getLanguages() as $language) {
             $this->addElement($this->createElement('text', $language, array(
                 'belongsTo' => 'title',
                 'label' => t('title') . " ($language)",
                 'filters' => array('StringTrim'),
-                'validators' => array(new Zend_Validate_NotEmpty(), $regex),
+                'validators' => array(new Zend_Validate_NotEmpty()),
             )));
         }
 
