ccaad11e1b9562355d71563d5fe29a71af63092a
jaapandre <jaapandre>
2011-12-01 12:28:38 +0000

    veilig voor oudere java (1.5 (zeg ik uit mijn hoofd) kan geen wildcard gebruiken om jars te includen

--- java/createReport.sh
+++ java/createReport.sh
@@ -4,7 +4,11 @@ echo ------ >> createReport.error
 echo $1 >> createReport.error
 echo ------ >> createReport.error
 
+jars=`find lib -name '*.jar'|tr "\n" ":"`
 
-java -cp .:./lib/* it.bisi.report.jasper.ExecuteReport $reportControlFile 2>>createReport.error
+echo "java -cp .:./lib/*:$jars it.bisi.report.jasper.ExecuteReport 127.0.0.1:$port/$db $dbUser $dbPass $reportId $dir 2>>createReport.error"
+java -cp .:./lib/*:$jars it.bisi.report.jasper.ExecuteReport 127.0.0.1:$port/$db $dbUser $dbPass $reportId $dir 2>>createReport.error
+
+//java -cp .:./lib/* it.bisi.report.jasper.ExecuteReport $reportControlFile 2>>createReport.error
 #more createReport.error
 
