312837b7b04a0705f451d67c9d98c49f4e24e8d8
jaapandre <jaapandre>
2011-12-02 11:30:18 +0000

    libraries ook voor java 1.5 includen (ergens is iets fout gegaan met commiten)

--- java/createReport.sh
+++ java/createReport.sh
@@ -6,9 +6,9 @@ echo ------ >> createReport.error
 
 jars=`find lib -name '*.jar'|tr "\n" ":"`
 
-echo "java -cp .:./lib/*:$jars it.bisi.report.jasper.ExecuteReport 127.0.0.1:$port/$db $dbUser $dbPass $reportId $dir 2>>createReport.error"
-java -cp .:./lib/*:$jars it.bisi.report.jasper.ExecuteReport 127.0.0.1:$port/$db $dbUser $dbPass $reportId $dir 2>>createReport.error
+//echo "java -cp .:./lib/*:$jars it.bisi.report.jasper.ExecuteReport 127.0.0.1:$port/$db $dbUser $dbPass $reportId $dir 2>>createReport.error"
 
-//java -cp .:./lib/* it.bisi.report.jasper.ExecuteReport $reportControlFile 2>>createReport.error
+java -cp .:./lib/* it.bisi.report.jasper.ExecuteReport $reportControlFile 2>>createReport.error
+java -cp .:./lib/*:$jars it.bisi.report.jasper.ExecuteReport $reportControlFile 2>>createReport.error
 #more createReport.error
 
