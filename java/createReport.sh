54a0ff8a9abf14816ec2825f561da5994b0a1ed2
jaapandre <jaapandre>
2011-12-05 12:35:48 +0000

    dubbele javac, een keer is genoeg

--- java/createReport.sh
+++ java/createReport.sh
@@ -8,7 +8,7 @@ jars=`find lib -name '*.jar'|tr "\n" ":"`
 
 #echo "java -cp .:./lib/*:$jars it.bisi.report.jasper.ExecuteReport 127.0.0.1:$port/$db $dbUser $dbPass $reportId $dir 2>>createReport.error"
 
-java -cp .:./lib/* it.bisi.report.jasper.ExecuteReport $reportControlFile 2>>createReport.error
+#java -cp .:./lib/* it.bisi.report.jasper.ExecuteReport $reportControlFile 2>>createReport.error
 java -cp .:./lib/*:$jars it.bisi.report.jasper.ExecuteReport $reportControlFile 2>>createReport.error
 #more createReport.error
 
