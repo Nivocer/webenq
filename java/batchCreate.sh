e6e8eca1b016afd8d09ab613121af27fafe94bd0
jaapandre <jaapandre>
2011-12-09 16:52:22 +0000

    mooi omgaan me tverschillende report control files

--- java/batchCreate.sh
+++ java/batchCreate.sh
@@ -1,9 +1,11 @@
 rm createReport.error
-reportControlFiles="/home/jaapandre/workspace/webenq4/java/reportControl.xml"
-reportControlFiles="/home/jaapandre/workspace/webenq4_hva_oo/java/reportControlHvA-rapport1.xml"
-#reportControlFiles="/home/jaapandre/workspace/webenq4_hva_oo/java/reportControlHvA-rapport2.xml"
-#reportControlFiles="/home/jaapandre/workspace/webenq4_hva_oo/java/reportControlHvA-rapport3.xml"
-#reportControlFiles="/home/jaapandre/workspace/webenq4_hva_oo/java/reportControlHvA-rapport4.xml"
+#reportControlFiles=$reportControlFiles" /home/jaapandre/workspace/webenq4_hva_oo/java/reportControl.xml"
+
+reportControlFiles=$reportControlFiles" /home/jaapandre/workspace/webenq4_hva_oo/java/reportControlHvA-rapport1.xml"
+#reportControlFiles=$reportControlFiles" /home/jaapandre/workspace/webenq4_hva_oo/java/reportControlHvA-rapport2.xml"
+#reportControlFiles=$reportControlFiles" /home/jaapandre/workspace/webenq4_hva_oo/java/reportControlHvA-rapport3.xml"
+#reportControlFiles=$reportControlFiles" /home/jaapandre/workspace/webenq4_hva_oo/java/reportControlHvA-rapport4.xml"
+
 
 for reportControlFile in `echo $reportControlFiles` 
 do 
