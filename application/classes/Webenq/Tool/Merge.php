13244ad4cd518f333a903a72fc3f319f0959782c
jaapandre <jaapandre>
2012-02-21 18:10:24 +0000

    merge 'normal' questback file, aan te passen om excel bestanden te koppelen. input *.zip, met spreadsheets, blad 1 worden gelijke kolommen samengevoegd

---
+++ application/classes/Webenq/Tool/Merge.php
@@ -0,0 +1,197 @@
+<?php
+/**
+ * Tool class for converting HVA data
+ *
+ * @package     Webenq
+ * @subpackage  Controllers
+ * @author      Bart Huttinga <b.huttinga@nivocer.com>
+ */
+class Webenq_Tool_Merge extends Webenq_Tool
+{
+    protected $_filename;
+
+    protected $_adapter;
+
+    /**
+     * @var array
+     */
+    protected $_data = array();
+
+    
+
+    /**
+     * The column number in which the respondent are stored
+     *
+     * @var int
+     */
+    protected $_respondentColumn;
+
+
+    /**
+     * @var array
+     */
+    protected $_respondents = array();
+
+    
+    /**
+     * The converted data
+     *
+     * @var array
+     */
+    protected $_newData = array();
+
+    protected $_firstRespondentId = 1;
+
+    public function __construct($filename)
+    {
+        $this->_filename = preg_replace('#(.*/)*#', null, $filename);
+        $this->_adapter = Webenq_Import_Adapter_Abstract::factory($filename);
+    }
+
+    public function process()
+    {
+        $this->_data = $this->getData();
+        $this->_respondents = $this->_getRespondents();
+        $this->_newData = $this->_getNewData();
+    }
+
+    public function getNewData()
+    {
+        return $this->_getNewData();
+    }
+
+    /**
+     * Returns a downloadable object
+     *
+     * @return Webenq_Download
+     */
+    public function getDownload()
+    {
+        // get first working sheet of new data
+        $data = $this->_newData[0];
+
+        $download = new Webenq_Download_Xls();
+        $download->setData($data)->init();
+        return $download;
+    }
+
+    public function getAdapter()
+    {
+        return $this->_adapter;
+    }
+
+    public function getData()
+    {
+        if (empty($this->_data)) {
+            $this->_data = $this->_adapter->getData();
+        }
+        return $this->_data;
+    }
+
+        /**
+     * Returns the respondents found in the data
+     *
+     * @return array Respondents
+     */
+    protected function _getRespondents()
+    {
+        $respondents = array();
+        foreach ($this->_data[0] as $i => $row) {
+            if ($i == 0) continue;
+            $respondents[] = $row[$this->_respondentColumn];
+        }
+        return $respondents;
+    }
+
+    /**
+     * Returns the column index that contains the respondents
+     *
+     * @return int
+     */
+    protected function _getRespondentColumn()
+    {
+        $respondentHeaderPattern = '/^Respondent$/';
+        foreach ($this->_data[0][0] as $respondentColumn => $header) {
+            if (preg_match($respondentHeaderPattern, $header)) {
+                return $respondentColumn;
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * Returns the new data
+     *
+     * @return array
+     */
+    protected function _getNewData()
+    {
+    	// get processed data
+    	$data = $this->_data;
+		
+    	//only for questback
+    	// get extra data from last working sheet
+    	$extraData = array();
+    	foreach ($data[2] as $row) {
+    		if (isset($row[0])) {
+    			$extraData[$row[0]] = $row[1];
+    		}
+    	}
+
+    	// copy headers from original data
+    	$new = array();
+    	$new[0][0] = $data[0][0];
+
+
+    	foreach ($data[0]  as $t_key=> $row) {
+    		if ($t_key==0) continue;
+    		// add some extra data (titel questionnaire, startdate, end date response)
+    		$row[] = $this->_firstRespondentId + $t_key;
+    		
+    		//check: only questback?
+    		foreach ($extraData as $key => $value) {
+    			$row[] = $value;	
+    		}
+    		
+    		$row[] = $this->_filename;
+    		
+    		$new[0][] = $row;
+    	}
+
+    		// add headers for extra data
+    		$new[0][0][] = '9999: Respondent ID';
+    		//check only for questback
+    		foreach ($extraData as $key => $value) $new[0][0][] = "9999: $key";
+    		
+    		$new[0][0][] = '9999: Filename';
+
+    		// add group numbers to questions, only for questback
+    		foreach ($new[0][0] as $key => $header) {
+    			if (!preg_match('/^\d+:\s/', $header)) {
+                $new[0][0][$key] = (1 + $key) . ': ' . $header;
+            }
+        }
+        //start only questback
+        // build the second working sheet, containing group information
+        $groups = array();
+        foreach ($new[0][0] as $header) {
+            if (preg_match('/^(\d+):\s(.+)$/', $header, $matches)) {
+                if ($matches[1] != 9999 && !key_exists($matches[1], $groups)) {
+                    $groups[$matches[1]] = $matches[2];
+                }
+            }
+        }
+        foreach ($groups as $key => $group) {
+            $row = $key - 1;
+            $new[1][$row][0] = "$key:  = $group";
+        }
+        $new[1][$row++][0] = '9999:  = Meta';
+
+        // simply copy third working sheet
+        $new[2] = $this->_data[2];
+        //end only for questback
+
+        return $new;
+    }
+}
--- application/controllers/ToolController.php
+++ application/controllers/ToolController.php
@@ -153,7 +153,156 @@ class ToolController extends Zend_Controller_Action
         $this->view->form = $form;
     }
 
+ public function mergeAction()
+    {
+        $form = new Webenq_Form_Tool_Hva();
+        $errors = array();
+        $lastRespondentId = 1;
+	$config = new Zend_Config_Ini(APPLICATION_PATH . "/configs/application.ini", APPLICATION_ENV);
 
+
+        if ($this->_helper->form->isPostedAndValid($form)) {
+
+            // make sure enough resources are assigned
+            try {
+		//add test if setMemory lower than 512M
+                //Webenq::setMemoryLimit('512M');
+                Webenq::setMaxExecutionTime(0);
+            } catch (Exception $e) {
+                $errors[] = $e->getMessage();
+            }
+
+            // receive the file
+            if ($form->file->receive()) {
+                $archiveInfo = $form->file->getFileInfo();
+                $parts = explode('.', $archiveInfo['file']['name']);
+                $extension = array_pop($parts);
+            } else {
+                $errors[] = 'Error receiving the file';
+            }
+
+            if (empty($errors)) {
+
+                // extract archive to tmp dir
+                $target = $archiveInfo['file']['destination']
+                    . '/' . md5(microtime(true) . mt_rand(0, 10000));
+                mkdir($target);
+                $filter = new Zend_Filter_Decompress(array(
+                	'adapter' => $extension,
+                	'options' => array('target' => $target)));
+
+                // process files
+                $data = array();
+                if ($filter->filter($archiveInfo['file']['tmp_name'])) {
+                    $filenames = scandir($target);
+                    foreach ($filenames as $filename) {
+
+                        // skip non-xls, non-xlsx and non-ods files
+                        if (!preg_match('/\.(xls|xlsx|ods)$/', strtolower($filename))) continue;
+
+                        $tool = new Webenq_Tool_Merge("$target/$filename");
+                        $tool->setFirstRespondentId($lastRespondentId);
+                        $tool->process();
+                        $data[] = $tool->getNewData();
+                        $lastRespondentId += $tool->countRespondents();
+
+                        unlink("$target/$filename");
+                    }
+                }
+                rmdir($target);
+
+                // calculate data for third working sheet
+                foreach ($data as $i => $set) {
+
+                    if (!isset($thirdWorkingSheet)) $thirdWorkingSheet = $set[2];
+
+                    $value = $set[2][1][1];
+                    if (isset($startDate)) {
+                        if ($startDate > new Zend_Date($value)) {
+                            $startDate = new Zend_Date($value);
+                        }
+                    } else {
+                        $startDate = new Zend_Date($value);
+                    }
+
+                    $value = $set[2][2][1];
+                    if (isset($endDate)) {
+                        if ($endDate < new Zend_Date($value)) {
+                            $endDate = new Zend_Date($value);
+                        }
+                    } else {
+                        $endDate = new Zend_Date($value);
+                    }
+
+                    $value = $set[2][3][1];
+                    if (isset($respondentCount)) {
+                        $respondentCount += $value;
+                    } else {
+                        $respondentCount = $value;
+                    }
+
+                    $value = $set[2][4][1];
+                    if (isset($emailInvitationCount)) {
+                        $emailInvitationCount += $value;
+                    } else {
+                        $emailInvitationCount = $value;
+                    }
+
+                    $value = $set[2][5][1];
+                    if (isset($emailResponseCount)) {
+                        $emailResponseCount += $value;
+                    } else {
+                        $emailResponseCount = $value;
+                    }
+
+                    $value = $set[2][6][1];
+                    if (isset($totalResponseCount)) {
+                        $totalResponseCount += $value;
+                    } else {
+                        $totalResponseCount = $value;
+                    }
+                }
+                $thirdWorkingSheet[0][1] = 'Module-evaluatie';
+                $thirdWorkingSheet[1][1] = $startDate->get('Y-MM-dd HH:mm:ss');
+                $thirdWorkingSheet[2][1] = $endDate->get('Y-MM-dd HH:mm:ss');
+                $thirdWorkingSheet[3][1] = $respondentCount;
+                $thirdWorkingSheet[4][1] = $emailInvitationCount;
+                $thirdWorkingSheet[5][1] = $emailResponseCount;
+                $thirdWorkingSheet[6][1] = $totalResponseCount;
+                if ($emailInvitationCount>0){
+                	$thirdWorkingSheet[7][1] = $emailResponseCount / $emailInvitationCount * 100;
+                }else {
+                	$thirdWorkingSheet[7][1] = '';
+                }
+                $thirdWorkingSheet[8][1] = 'divers';
+
+
+                
+                //merge data:
+                foreach ($data as $setId => $dataSheet){
+                	if ($setId ===0) continue;
+                 	$data[0]=$this->_mergeData($data[0],$dataSheet);
+                 	unset ($data[$setId]);
+                }
+                
+                // disabled layout and viewRenderer
+                $this->_helper->layout->disableLayout();
+                $this->_helper->viewRenderer->setNoRender();
+
+                // return file for download
+                $download = new Webenq_Download_Xls();
+                $download->setData($data[0][0])->init();
+                $download->addWorkingSheet($data[0][1]);
+                $download->addWorkingSheet($thirdWorkingSheet);
+                $download->save($archiveInfo['file']['name']);
+            }
+        }
+
+        $this->view->errors = $errors;
+        $this->view->form = $form;
+    }
+    
+    
     /**
      * Merges data array if the question texts are the same
      * 
@@ -191,6 +340,7 @@ class ToolController extends Zend_Controller_Action
        		if (count($questionsClean)<>count($questions)){
        			//questions are not unique!!!!
        			echo 'questions are not unique';
+       			break;
        		}
        		//create array with questionsNew as value and 'column' as key 
     		foreach ($questionsNew as $column => &$question) {
@@ -202,10 +352,12 @@ class ToolController extends Zend_Controller_Action
     				$questionsNewClean[$column]=$question;
     			}
        		}
-       		if (count($questionsClean)<>count($questions)){
+       		if (count($questionsNewClean)<>count($questionsNew)){
        			//questions are not unique!!!!
-       			echo 'questions are not unique';
+       			echo 'questions new are not unique';
+       			break;
        			return false;
+       			
        		}
        		
        		
