20a13caefe7a84293595f6c4291b3dbc59c78614
jaapandre <jaapandre>
2012-02-23 14:07:32 +0000

    [5627][5582][5581][5574] aansturen van de verschillende controlfiles

--- java/batchCreate.sh
+++ java/batchCreate.sh
@@ -1,10 +1,24 @@
 rm createReport.error
 #reportControlFiles=$reportControlFiles" /home/jaapandre/workspace/webenq4_hva_oo/java/reportControl.xml"
 
-reportControlFiles=$reportControlFiles" /home/jaapandre/workspace/webenq4_hva_oo/java/reportControlHvA-rapport1.xml"
+#reportControlFiles=$reportControlFiles" /home/jaapandre/workspace/webenq4_hva_oo/java/reportControlHvA-rapport1.xml"
 #reportControlFiles=$reportControlFiles" /home/jaapandre/workspace/webenq4_hva_oo/java/reportControlHvA-rapport2.xml"
 #reportControlFiles=$reportControlFiles" /home/jaapandre/workspace/webenq4_hva_oo/java/reportControlHvA-rapport3.xml"
 #reportControlFiles=$reportControlFiles" /home/jaapandre/workspace/webenq4_hva_oo/java/reportControlHvA-rapport4.xml"
+
+#reportControlFiles=$reportControlFiles" http://demo4.webenq.org/report/control/id/1"
+#reportControlFiles=$reportControlFiles" http://demo4.webenq.org/report/control/id/2"
+#reportControlFiles=$reportControlFiles" http://demo4.webenq.org/report/control/id/3"
+#reportControlFiles=$reportControlFiles" http://demo4.webenq.org/report/control/id/4"
+
+#rapport 1 gedefinieerd via webenq.org
+#reportControlFiles=$reportControlFiles" ./semester1-reportControlHvA-rapport1test.xml"
+
+#hvaoo: semester 2011-2012 semester 1:
+#reportControlFiles=$reportControlFiles" ./semester1-reportControlHvA-rapport1.xml"
+#reportControlFiles=$reportControlFiles" ./semester1-reportControlHvA-rapport2.xml"
+#reportControlFiles=$reportControlFiles" ./semester1-reportControlHvA-rapport3.xml"
+reportControlFiles=$reportControlFiles" ./semester1-reportControlHvA-rapport4.xml"
 
 
 for reportControlFile in `echo $reportControlFiles` 
