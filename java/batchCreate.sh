1250dba79b48d0c090089b8f05723210877dad80
jaapandre <jaapandre>
2012-03-15 13:25:19 +0000

    nog wat leuke aanstuurfiles in batchCreate.sh

--- java/batchCreate.sh
+++ java/batchCreate.sh
@@ -18,8 +18,28 @@ rm createReport.error
 #reportControlFiles=$reportControlFiles" ./semester1-reportControlHvA-rapport1.xml"
 #reportControlFiles=$reportControlFiles" ./semester1-reportControlHvA-rapport2.xml"
 #reportControlFiles=$reportControlFiles" ./semester1-reportControlHvA-rapport3.xml"
-reportControlFiles=$reportControlFiles" ./semester1-reportControlHvA-rapport4.xml"
+#reportControlFiles=$reportControlFiles" ./semester1-reportControlHvA-rapport4.xml"
 
+#reportControlFiles=$reportControlFiles" ./semester1-reportControlHvA-rapport6.xml"
+
+#reportControlFiles=$reportControlFiles" http://webenq4.localhost/report/control/id/9"
+#reportControlFiles=$reportControlFiles" http://webenq4.localhost/report/control/id/10"
+#reportControlFiles=$reportControlFiles" http://webenq4.localhost/report/control/id/11"
+#reportControlFiles=$reportControlFiles" http://webenq4.localhost/report/control/id/12"
+
+#werkplekleren
+#reportControlFiles=$reportControlFiles" ./semester1-reportControlHvA-rapport1Werkplekleren.xml"
+reportControlFiles=$reportControlFiles" ./semester1-reportControlHvA-rapport2Werkplekleren.xml"
+#reportControlFiles=$reportControlFiles" ./semester1-reportControlHvA-rapport3Werkplekleren.xml"
+#semester1-reportControlHvA-rapport1Werkplekleren.xml 
+
+#pabo blok 1
+#response
+#reportControlFiles=$reportControlFiles" http://webenq4.localhost/report/control/id/15"
+#reportControlFiles=$reportControlFiles"  semester1-blok1-reportControlHva-rapport6.xml"
+
+
+#reportControlFiles=$reportControlFiles" http://webenq4.localhost/report/control/id/17"
 
 for reportControlFile in `echo $reportControlFiles` 
 do 
