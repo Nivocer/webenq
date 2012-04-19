b12895826e41dd17434a06cecbb0d6a4cf5e5ca4
Bart Huttinga <info@bhapp.nl>
2012-04-19 17:48:01 +0200

    Fix YAML after Doctrine dump

--- bin/doctrine.php
+++ bin/doctrine.php
@@ -7,6 +7,15 @@ define('APPLICATION_ENV', 'development');
 // initialize
 require_once realpath(dirname(__FILE__) . '/../public/init.php');
 $application->bootstrap('doctrine');
+$configuration = $application->getOption('doctrine');
 
-$cli = new Doctrine_Cli($application->getOption('doctrine'));
+// run
+$cli = new Doctrine_Cli($configuration);
 $cli->run($_SERVER['argv']);
+
+// fix dumped YAML
+if (isset($_SERVER['argv'][1]) && $_SERVER['argv'][1] == 'dump-data') {
+    require_once realpath(dirname(__FILE__) . '/fixYaml.php');
+    $fixYaml = new FixYaml($configuration['data_fixtures_path']);
+    $fixYaml->run();
+}
---
+++ bin/fixYaml.php
@@ -0,0 +1,43 @@
+<?php
+class FixYaml
+{
+    protected $_directory;
+
+    public function __construct($directory)
+    {
+        $this->_directory = $directory;
+    }
+
+    public function run()
+    {
+        $yamlFiles = array();
+        if (is_dir($this->_directory)) {
+            foreach (scandir($this->_directory) as $file) {
+                if (preg_match('/\.yml$/', $file)) {
+                    $yamlFiles[] = $file;
+                }
+            }
+        }
+
+        foreach ($yamlFiles as $file) {
+            $yaml = file_get_contents($this->_directory . '/' . $file);
+
+            $yaml = preg_replace("/    answerPossibilityGroup_id: '(\d*)'/",
+                "    AnswerPossibilityGroup: Webenq_Model_AnswerPossibilityGroup_$1", $yaml);
+
+            $yaml = preg_replace("/    answerPossibilityGroup_id: null\n/",
+                null, $yaml);
+
+            $yaml = preg_replace("/    answerPossibility_id: '(\d*)'\n/",
+                    "    AnswerPossibility: Webenq_Model_AnswerPossibility_$1\n", $yaml);
+
+            $yaml = preg_replace("/    answerPossibilityText_id: '(\d*)'\n/",
+                    "    AnswerPossibilityText: Webenq_Model_AnswerPossibilityText_$1\n", $yaml);
+
+            $yaml = preg_replace("/    Children: Webenq_Model_CollectionPresentation_(\d*)\n/",
+                    "    Children: [Webenq_Model_CollectionPresentation_$1]\n", $yaml);
+
+            file_put_contents($this->_directory . '/' . $file, $yaml);
+        }
+    }
+}
