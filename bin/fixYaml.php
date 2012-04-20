2bb3d0c0216d4c7f214ce4f7a6033d0ef6ffa781
Jaap-Andre de Hoop <j.dehoop@nivocer.com>
2012-04-20 10:31:18 +0200

    add some documentation

--- bin/fixYaml.php
+++ bin/fixYaml.php
@@ -1,4 +1,9 @@
 <?php
+/* 
+ script to fix the yaml output of doctrine dump-data, so we can load it with doctrine load-data
+
+*/
+
 class FixYaml
 {
     protected $_directory;
@@ -21,19 +26,22 @@ class FixYaml
 
         foreach ($yamlFiles as $file) {
             $yaml = file_get_contents($this->_directory . '/' . $file);
-
+            //fix foreign key reference
             $yaml = preg_replace("/    answerPossibilityGroup_id: '(\d*)'/",
                 "    AnswerPossibilityGroup: Webenq_Model_AnswerPossibilityGroup_$1", $yaml);
 
             $yaml = preg_replace("/    answerPossibilityGroup_id: null\n/",
                 null, $yaml);
 
+            //fix foreign key reference
             $yaml = preg_replace("/    answerPossibility_id: '(\d*)'\n/",
                     "    AnswerPossibility: Webenq_Model_AnswerPossibility_$1\n", $yaml);
 
+            //fix foreign key reference
             $yaml = preg_replace("/    answerPossibilityText_id: '(\d*)'\n/",
                     "    AnswerPossibilityText: Webenq_Model_AnswerPossibilityText_$1\n", $yaml);
 
+            //fix one-many relationship
             $yaml = preg_replace("/    Children: Webenq_Model_CollectionPresentation_(\d*)\n/",
                     "    Children: [Webenq_Model_CollectionPresentation_$1]\n", $yaml);
 
