073424d881f3ee975552291f3521e7bfd7454146
Rolf Kleef <r.kleef@nivocer.com>
2012-05-10 17:30:55 +0200

    Fix 5751: changed test suite settings to output to directory /public/tests, eliminating symlink

--- tests/pdepend.sh
+++ tests/pdepend.sh
@@ -1,1 +1,1 @@
-pdepend  --optimization=best --suffix=php --summary-xml=$PWD/log/summary.xml --jdepend-chart=$PWD/log/jdepend.svg --overview-pyramid=$PWD/log/pyramid.svg ../application/
+pdepend  --optimization=best --suffix=php --summary-xml=$PWD/../public/tests/log/summary.xml --jdepend-chart=$PWD/../public/tests/log/jdepend.svg --overview-pyramid=$PWD/../public/tests/log/pyramid.svg ../application/
