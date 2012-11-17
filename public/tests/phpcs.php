f058bedc54eacfed535b57767ba1bc1fe4957226
Jaap-Andre de Hoop <j.dehoop@nivocer.com>
2012-11-17 16:56:47 +0100

    add summary information
    Change-Id: I9af8c5c4da45beb9a503a40081904afcbca741ff

--- public/tests/phpcs.php
+++ public/tests/phpcs.php
@@ -3,7 +3,7 @@
 $xml = simplexml_load_file("log/phpcs.xml");
 
 $files = array();
-foreach($xml->file as $file) {
+foreach ($xml->file as $file) {
   $file['name'] = substr($file['name'], strpos($file['name'], 'Joobsbox/'));
   $files[] = $file;
 }
@@ -20,79 +20,96 @@ foreach($xml->file as $file) {
 	    font-family: "Corbel", "Myriad Pro", "Calibri", "Verdana", "Helvetica", "Arial", sans-serif;
 	    text-align: center;
 	  }
-	  
+
 	  table {
 	    font-size: 13px;
 	    text-align: left;
 	    margin: 0 auto;
 	  }
-	  
+
 	  table th {
 	    min-width: 100px;
 	    padding: 5px;
 	  }
-	  
+
 	  table tbody tr:nth-child(2n) td {
 	    background-color: #EEE;
 	  }
-	  
+
 	  table thead th {
 	    background-color: #444;
 	    color: #FFF;
 	    padding: 5px 10px;
 	    text-align: center;
 	  }
-	  
+
 	  table thead th:first-child {
 	    -moz-border-radius-topleft: 10px;
 	  }
-	  
+
 	  table thead th:last-child {
 	    -moz-border-radius-topright: 10px;
 	  }
-	  
+
 	  table td {
 	    padding: 3px 10px;
 	    border-right: 1px solid #999;
 	    border-left: 1px solid #999;
 	  }
-	  
+
 	  table td.right {
 	    text-align: right;
 	  }
-	  
+
 	  table {
 	    border-collapse: collapse;
 	    border-bottom: 1px solid #999;
 	  }
-	  
+
 	  ul, ul li {
 	    list-style: none;
 	    margin: 0;
 	    padding: 0;
 	  }
-	  
+
 	  .file-name {
 	    text-align: center;
 	    font-size: 16px;
 	    padding: 7px;
 	    margin: 0;
 	  }
-	  
+
 	  .file {
 	      margin: 10px 0;
 	  }
-	  
+
 	  .ok {
 	      background-color: #8CCA7C;
+	  }
+	  .notOk {
+	      background-color: #EB2F2F;
 	  }
 	</style>
 </head>
 
 <body>
   <h1>PHP CodeSniffer Report</h1>
-  
+
   <ul id="files">
+  <?php foreach($files as $file):
+    $numberOfFiles++;
+    if (count($file->error) == 0) {
+        $correctFiles++;
+    } else {
+        $incorrectFiles++;
+        $numberOfErrors=$numberOfErrors+count($file->error);
+    }
+    endforeach;
+    echo "Total number of scanned files: $numberOfFiles <br/>";
+    echo "<span class=\"ok\">Total number of correct files: $correctFiles (". (round($correctFiles/$numberOfFiles*100))."%)<br/></span>";
+    echo "<span class=\"notOk\">Total number of incorrect files: $incorrectFiles (". (round($incorrectFiles/$numberOfFiles*100))."%)<br/></span>";
+    echo "Total number of errors: $numberOfErrors <br/>";
+  ?>
   <?php foreach($files as $file): ?>
     <li class="file <?php if(count($file->error) == 0): echo 'ok'; endif; ?>">
       <div class="file-name"><?php echo $file['name']; ?></div>
