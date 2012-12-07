0d9f6972054316571dcfbaeb18753abf334905df
Rolf Kleef <r.kleef@nivocer.com>
2012-12-07 19:36:15 +0100

    Updated wireframes with screens to "manage questionnaires"

--- wireframes/index_files/story.js
+++ wireframes/index_files/story.js
@@ -6,19 +6,30 @@ var story = {
    "height": 364,
    "width": 958,
    "image": "1-login.png",
-   "links": [{
-    "page": 1,
-    "rect": [
-     140,
-     310,
-     317,
-     354
-    ]
-   }]
+   "links": [
+    {
+     "page": 4,
+     "rect": [
+      21,
+      313,
+      110,
+      344
+     ]
+    },
+    {
+     "page": 1,
+     "rect": [
+      140,
+      310,
+      352,
+      362
+     ]
+    }
+   ]
   },
   {
    "title": "login-error",
-   "height": 257,
+   "height": 269,
    "width": 958,
    "image": "2-login-error.png",
    "links": [{
@@ -26,10 +37,202 @@ var story = {
     "rect": [
      26,
      196,
-     233,
-     257
+     268,
+     269
     ]
    }]
+  },
+  {
+   "title": "add-questionnaire",
+   "height": 457,
+   "width": 617,
+   "image": "3-add-questionnaire.png",
+   "links": [
+    {
+     "page": 4,
+     "rect": [
+      34,
+      409,
+      110,
+      440
+     ]
+    },
+    {
+     "page": 4,
+     "rect": [
+      127,
+      409,
+      188,
+      440
+     ]
+    }
+   ]
+  },
+  {
+   "title": "edit-questionnaire-properties",
+   "height": 457,
+   "width": 617,
+   "image": "4-edit-questionnaire-properties.png",
+   "links": [
+    {
+     "page": 4,
+     "rect": [
+      34,
+      409,
+      110,
+      440
+     ]
+    },
+    {
+     "page": 4,
+     "rect": [
+      127,
+      409,
+      188,
+      440
+     ]
+    }
+   ]
+  },
+  {
+   "title": "manage-questionnaires-overview",
+   "height": 343,
+   "width": 958,
+   "image": "5-manage-questionnaires-overview.png",
+   "links": [
+    {
+     "page": 2,
+     "rect": [
+      3,
+      312,
+      188,
+      343
+     ]
+    },
+    {
+     "page": 3,
+     "rect": [
+      414,
+      264,
+      431,
+      292
+     ]
+    },
+    {
+     "page": 5,
+     "rect": [
+      120,
+      264,
+      353,
+      292
+     ]
+    },
+    {
+     "page": 4,
+     "rect": [
+      204,
+      0,
+      336,
+      37
+     ]
+    },
+    {
+     "page": 3,
+     "rect": [
+      414,
+      235,
+      431,
+      263
+     ]
+    },
+    {
+     "page": 5,
+     "rect": [
+      120,
+      235,
+      353,
+      263
+     ]
+    },
+    {
+     "page": 3,
+     "rect": [
+      414,
+      206,
+      431,
+      234
+     ]
+    },
+    {
+     "page": 5,
+     "rect": [
+      120,
+      206,
+      353,
+      234
+     ]
+    },
+    {
+     "page": 6,
+     "rect": [
+      3,
+      207,
+      25,
+      232
+     ]
+    },
+    {
+     "page": 6,
+     "rect": [
+      3,
+      236,
+      25,
+      261
+     ]
+    },
+    {
+     "page": 6,
+     "rect": [
+      2,
+      265,
+      24,
+      290
+     ]
+    }
+   ]
+  },
+  {
+   "title": "preview-questionnaire",
+   "height": 218,
+   "width": 958,
+   "image": "6-preview-questionnaire.png",
+   "links": [
+    {
+     "page": 4,
+     "rect": [
+      122,
+      166,
+      378,
+      218
+     ]
+    },
+    {
+     "page": 4,
+     "rect": [
+      204,
+      0,
+      336,
+      37
+     ]
+    }
+   ]
+  },
+  {
+   "title": "hint-drag-to-move",
+   "height": 120,
+   "width": 286,
+   "image": "7-hint-drag-to-move.png",
+   "links": []
   }
  ],
  "highlightLinks": false
