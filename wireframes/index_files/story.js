46701f827d4649a714c70b88b063f0ccbfc77f12
Jaap-Andre de Hoop <j.dehoop@nivocer.com>
2012-12-18 12:14:06 +0100

    add screens for manage questionnaire https://www.pivotaltracker.com/story/show/41322207 adjust menu, add management-item, with submenu, moved questions, answers to submenu

--- wireframes/index_files/story.js
+++ wireframes/index_files/story.js
@@ -8,12 +8,12 @@ var story = {
    "image": "1-login.png",
    "links": [
     {
-     "page": 8,
+     "page": 2,
      "rect": [
       21,
       313,
-      90,
-      344
+      70,
+      336
      ]
     },
     {
@@ -21,8 +21,8 @@ var story = {
      "rect": [
       140,
       310,
-      352,
-      362
+      269,
+      346
      ]
     },
     {
@@ -30,12 +30,12 @@ var story = {
      "rect": [
       145,
       0,
-      204,
-      37
+      184,
+      23
      ]
     },
     {
-     "page": 7,
+     "page": 10,
      "rect": [
       898,
       0,
@@ -44,7 +44,7 @@ var story = {
      ]
     },
     {
-     "page": 7,
+     "page": 10,
      "rect": [
       932,
       0,
@@ -56,7 +56,7 @@ var story = {
   },
   {
    "title": "login-error",
-   "height": 269,
+   "height": 245,
    "width": 958,
    "image": "2-login-error.png",
    "links": [
@@ -65,12 +65,12 @@ var story = {
      "rect": [
       145,
       0,
-      204,
-      37
+      184,
+      23
      ]
     },
     {
-     "page": 7,
+     "page": 10,
      "rect": [
       898,
       0,
@@ -79,7 +79,7 @@ var story = {
      ]
     },
     {
-     "page": 7,
+     "page": 10,
      "rect": [
       932,
       0,
@@ -92,8 +92,178 @@ var story = {
      "rect": [
       26,
       196,
-      268,
-      269
+      174,
+      245
+     ]
+    }
+   ]
+  },
+  {
+   "title": "manage-questionnaires-overview",
+   "height": 338,
+   "width": 958,
+   "image": "3-manage-questionnaires-overview.png",
+   "links": [
+    {
+     "page": 8,
+     "rect": [
+      31,
+      207,
+      138,
+      291
+     ]
+    },
+    {
+     "page": 3,
+     "rect": [
+      3,
+      312,
+      128,
+      338
+     ]
+    },
+    {
+     "page": 4,
+     "rect": [
+      441,
+      264,
+      465,
+      292
+     ]
+    },
+    {
+     "page": 5,
+     "rect": [
+      144,
+      264,
+      401,
+      292
+     ]
+    },
+    {
+     "page": 11,
+     "rect": [
+      145,
+      0,
+      184,
+      23
+     ]
+    },
+    {
+     "page": 2,
+     "rect": [
+      184,
+      0,
+      265,
+      23
+     ]
+    },
+    {
+     "page": 12,
+     "rect": [
+      306,
+      0,
+      379,
+      23
+     ]
+    },
+    {
+     "page": 0,
+     "rect": [
+      417,
+      0,
+      463,
+      23
+     ]
+    },
+    {
+     "page": 10,
+     "rect": [
+      898,
+      0,
+      933,
+      37
+     ]
+    },
+    {
+     "page": 10,
+     "rect": [
+      932,
+      0,
+      958,
+      37
+     ]
+    },
+    {
+     "page": 4,
+     "rect": [
+      441,
+      235,
+      465,
+      263
+     ]
+    },
+    {
+     "page": 5,
+     "rect": [
+      144,
+      235,
+      401,
+      263
+     ]
+    },
+    {
+     "page": 4,
+     "rect": [
+      441,
+      206,
+      465,
+      234
+     ]
+    },
+    {
+     "page": 5,
+     "rect": [
+      144,
+      206,
+      401,
+      234
+     ]
+    },
+    {
+     "page": 9,
+     "rect": [
+      2,
+      207,
+      32,
+      232
+     ]
+    },
+    {
+     "page": 9,
+     "rect": [
+      2,
+      236,
+      32,
+      261
+     ]
+    },
+    {
+     "page": 9,
+     "rect": [
+      2,
+      265,
+      32,
+      290
+     ]
+    },
+    {
+     "page": 6,
+     "rect": [
+      24,
+      179,
+      138,
+      207
      ]
     }
    ]
@@ -102,24 +272,24 @@ var story = {
    "title": "add-questionnaire",
    "height": 497,
    "width": 641,
-   "image": "3-add-questionnaire.png",
+   "image": "4-add-questionnaire.png",
    "links": [
     {
-     "page": 4,
+     "page": 2,
      "rect": [
       58,
       449,
-      134,
-      480
+      111,
+      472
      ]
     },
     {
-     "page": 4,
+     "page": 2,
      "rect": [
       151,
       449,
-      212,
-      480
+      195,
+      472
      ]
     }
    ]
@@ -128,215 +298,81 @@ var story = {
    "title": "edit-questionnaire-properties",
    "height": 497,
    "width": 641,
-   "image": "4-edit-questionnaire-properties.png",
-   "links": [
-    {
-     "page": 4,
-     "rect": [
-      58,
-      449,
-      134,
-      480
-     ]
-    },
-    {
-     "page": 4,
-     "rect": [
-      151,
-      449,
-      212,
-      480
-     ]
-    }
-   ]
-  },
-  {
-   "title": "manage-questionnaires-overview",
-   "height": 343,
-   "width": 958,
-   "image": "5-manage-questionnaires-overview.png",
+   "image": "5-edit-questionnaire-properties.png",
    "links": [
     {
      "page": 2,
      "rect": [
-      3,
-      312,
-      188,
-      343
+      58,
+      449,
+      111,
+      472
      ]
     },
     {
-     "page": 3,
+     "page": 2,
      "rect": [
-      414,
-      264,
-      431,
-      292
-     ]
-    },
-    {
-     "page": 5,
-     "rect": [
-      120,
-      264,
-      353,
-      292
-     ]
-    },
-    {
-     "page": 8,
-     "rect": [
-      145,
-      0,
-      204,
-      37
-     ]
-    },
-    {
-     "page": 4,
-     "rect": [
-      204,
-      0,
-      336,
-      37
-     ]
-    },
-    {
-     "page": 0,
-     "rect": [
-      626,
-      0,
-      696,
-      37
-     ]
-    },
-    {
-     "page": 7,
-     "rect": [
-      898,
-      0,
-      933,
-      37
-     ]
-    },
-    {
-     "page": 7,
-     "rect": [
-      932,
-      0,
-      958,
-      37
-     ]
-    },
-    {
-     "page": 3,
-     "rect": [
-      414,
-      235,
-      431,
-      263
-     ]
-    },
-    {
-     "page": 5,
-     "rect": [
-      120,
-      235,
-      353,
-      263
-     ]
-    },
-    {
-     "page": 3,
-     "rect": [
-      414,
-      206,
-      431,
-      234
-     ]
-    },
-    {
-     "page": 5,
-     "rect": [
-      120,
-      206,
-      353,
-      234
-     ]
-    },
-    {
-     "page": 6,
-     "rect": [
-      3,
-      207,
-      25,
-      232
-     ]
-    },
-    {
-     "page": 6,
-     "rect": [
-      3,
-      236,
-      25,
-      261
-     ]
-    },
-    {
-     "page": 6,
-     "rect": [
-      2,
-      265,
-      24,
-      290
+      151,
+      449,
+      195,
+      472
      ]
     }
    ]
   },
   {
    "title": "preview-questionnaire",
-   "height": 218,
+   "height": 202,
    "width": 958,
    "image": "6-preview-questionnaire.png",
    "links": [
     {
-     "page": 4,
+     "page": 6,
      "rect": [
       122,
       166,
-      378,
-      218
+      278,
+      202
      ]
     },
     {
-     "page": 8,
+     "page": 11,
      "rect": [
       145,
       0,
-      204,
-      37
+      184,
+      23
      ]
     },
     {
-     "page": 4,
+     "page": 2,
      "rect": [
-      204,
+      184,
       0,
-      336,
-      37
+      265,
+      23
+     ]
+    },
+    {
+     "page": 12,
+     "rect": [
+      306,
+      0,
+      379,
+      23
      ]
     },
     {
      "page": 0,
      "rect": [
-      626,
+      417,
       0,
-      696,
-      37
+      463,
+      23
      ]
     },
     {
-     "page": 7,
+     "page": 10,
      "rect": [
       898,
       0,
@@ -345,7 +381,7 @@ var story = {
      ]
     },
     {
-     "page": 7,
+     "page": 10,
      "rect": [
       932,
       0,
@@ -356,54 +392,67 @@ var story = {
    ]
   },
   {
-   "title": "hint-drag-to-move",
-   "height": 120,
-   "width": 301,
-   "image": "7-hint-drag-to-move.png",
-   "links": []
-  },
-  {
-   "title": "hint-switch-interface-language",
-   "height": 185,
-   "width": 364,
-   "image": "8-hint-switch-interface-language.png",
-   "links": []
-  },
-  {
-   "title": "home",
-   "height": 200,
+   "title": "manage-categories-overview",
+   "height": 338,
    "width": 958,
-   "image": "9-home.png",
+   "image": "7-manage-categories-overview.png",
    "links": [
     {
-     "page": 8,
+     "page": 7,
      "rect": [
-      145,
-      0,
-      205,
-      39
+      3,
+      312,
+      105,
+      338
      ]
     },
     {
-     "page": 4,
+     "page": 8,
      "rect": [
-      205,
+      24,
+      244,
+      225,
+      268
+     ]
+    },
+    {
+     "page": 11,
+     "rect": [
+      145,
       0,
-      345,
-      39
+      184,
+      23
+     ]
+    },
+    {
+     "page": 2,
+     "rect": [
+      184,
+      0,
+      265,
+      23
+     ]
+    },
+    {
+     "page": 12,
+     "rect": [
+      306,
+      0,
+      379,
+      23
      ]
     },
     {
      "page": 0,
      "rect": [
-      647,
+      417,
       0,
-      723,
-      39
+      463,
+      23
      ]
     },
     {
-     "page": 7,
+     "page": 10,
      "rect": [
       898,
       0,
@@ -412,12 +461,274 @@ var story = {
      ]
     },
     {
-     "page": 7,
+     "page": 10,
      "rect": [
       932,
       0,
       958,
       37
+     ]
+    },
+    {
+     "page": 8,
+     "rect": [
+      24,
+      217,
+      225,
+      245
+     ]
+    },
+    {
+     "page": 8,
+     "rect": [
+      24,
+      193,
+      225,
+      218
+     ]
+    },
+    {
+     "page": 9,
+     "rect": [
+      3,
+      194,
+      25,
+      219
+     ]
+    },
+    {
+     "page": 9,
+     "rect": [
+      3,
+      218,
+      25,
+      243
+     ]
+    },
+    {
+     "page": 9,
+     "rect": [
+      3,
+      242,
+      25,
+      267
+     ]
+    }
+   ]
+  },
+  {
+   "title": "add-category",
+   "height": 497,
+   "width": 641,
+   "image": "8-add-category.png",
+   "links": [
+    {
+     "page": 6,
+     "rect": [
+      55,
+      384,
+      108,
+      407
+     ]
+    },
+    {
+     "page": 6,
+     "rect": [
+      152,
+      384,
+      196,
+      407
+     ]
+    }
+   ]
+  },
+  {
+   "title": "edit-category",
+   "height": 497,
+   "width": 641,
+   "image": "9-edit-category.png",
+   "links": [
+    {
+     "page": 6,
+     "rect": [
+      58,
+      453,
+      111,
+      476
+     ]
+    },
+    {
+     "page": 6,
+     "rect": [
+      152,
+      453,
+      196,
+      476
+     ]
+    },
+    {
+     "page": 4,
+     "rect": [
+      61,
+      384,
+      213,
+      425
+     ]
+    }
+   ]
+  },
+  {
+   "title": "hint-drag-to-move",
+   "height": 111,
+   "width": 197,
+   "image": "10-hint-drag-to-move.png",
+   "links": []
+  },
+  {
+   "title": "hint-switch-interface-language",
+   "height": 176,
+   "width": 260,
+   "image": "11-hint-switch-interface-language.png",
+   "links": []
+  },
+  {
+   "title": "home",
+   "height": 191,
+   "width": 958,
+   "image": "12-home.png",
+   "links": [
+    {
+     "page": 11,
+     "rect": [
+      145,
+      0,
+      184,
+      23
+     ]
+    },
+    {
+     "page": 2,
+     "rect": [
+      184,
+      0,
+      268,
+      23
+     ]
+    },
+    {
+     "page": 12,
+     "rect": [
+      310,
+      0,
+      384,
+      23
+     ]
+    },
+    {
+     "page": 0,
+     "rect": [
+      421,
+      0,
+      467,
+      23
+     ]
+    },
+    {
+     "page": 10,
+     "rect": [
+      898,
+      0,
+      933,
+      37
+     ]
+    },
+    {
+     "page": 10,
+     "rect": [
+      932,
+      0,
+      958,
+      37
+     ]
+    }
+   ]
+  },
+  {
+   "title": "home-menu-management-extend",
+   "height": 114,
+   "width": 958,
+   "image": "13-home-menu-management-extend.png",
+   "links": [
+    {
+     "page": 11,
+     "rect": [
+      145,
+      0,
+      184,
+      23
+     ]
+    },
+    {
+     "page": 2,
+     "rect": [
+      184,
+      0,
+      268,
+      23
+     ]
+    },
+    {
+     "page": 12,
+     "rect": [
+      310,
+      0,
+      384,
+      23
+     ]
+    },
+    {
+     "page": 0,
+     "rect": [
+      421,
+      0,
+      467,
+      23
+     ]
+    },
+    {
+     "page": 10,
+     "rect": [
+      898,
+      0,
+      933,
+      37
+     ]
+    },
+    {
+     "page": 10,
+     "rect": [
+      932,
+      0,
+      958,
+      37
+     ]
+    },
+    {
+     "page": 6,
+     "rect": [
+      297,
+      24,
+      399,
+      46
+     ]
+    },
+    {
+     "page": 2,
+     "rect": [
+      297,
+      46,
+      399,
+      68
      ]
     }
    ]
