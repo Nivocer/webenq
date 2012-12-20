12086e2d11fef88770bb38ac8f3a5922407bb027
Jaap-Andre de Hoop <j.dehoop@nivocer.com>
2012-12-20 15:27:45 +0100

    reactivated and minor adjustments of the userControllerTest

--- tests/application/controllers/UserControllerTest.php
+++ tests/application/controllers/UserControllerTest.php
@@ -1,26 +1,35 @@
 <?php
 class Webenq_Test_ControllerTestCase_UserControllerTest extends Webenq_Test_Case_Controller
 {
-    public function testDummy() {
+    public function testLoginFormIsRendered()
+    {
+        $this->dispatch('/user/login');
+
+        $this->assertController('user');
+        $this->assertAction('login');
+        $this->assertQuery('input#username');
+        $this->assertQuery('input#password');
     }
 
-//    public function testLoginFormIsRendered()
-//    {
-//        $this->dispatch('user/login');
-//        $this->assertQuery('input#username');
-//        $this->assertQuery('input#password');
-//    }
-//
-//    public function testUserCanLoginAndLogout()
-//    {
-//        $this->getRequest()->setMethod('POST')->setPost(array(
-//            'username' => 'test',
-//            'password' => 'test'
-//        ));
-//        $this->dispatch('user/login');
-//        $this->assertTrue(Zend_Auth::getInstance()->hasIdentity());
-//
-//        $this->dispatch('user/logout');
-//        $this->assertFalse(Zend_Auth::getInstance()->hasIdentity());
-//    }
+    public function testUserCanLoginAndLogout()
+    {
+        $this->getRequest()->setMethod('POST')->setPost(array(
+            'username' => 'invalidxyz',
+            'password' => 'invalidxyz'
+        ));
+        $this->dispatch('user/login');
+        $this->assertFalse(Zend_Auth::getInstance()->hasIdentity(), "should not be able to login as 'invalidxyz'");
+
+
+        $this->getRequest()->setMethod('POST')->setPost(array(
+                'username' => 'admin',
+                'password' => 'webenq'
+        ));
+
+        $this->dispatch('user/login');
+        $this->assertTrue(Zend_Auth::getInstance()->hasIdentity(),"should be able to login 'admin'");
+
+        $this->dispatch('user/logout');
+        $this->assertFalse(Zend_Auth::getInstance()->hasIdentity(), "unable to logout");
+    }
 }
