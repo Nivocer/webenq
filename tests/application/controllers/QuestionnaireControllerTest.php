8ae44aff6d4f4bae7fee45258c3605b7e5444601
Jaap-Andre de Hoop <j.dehoop@nivocer.com>
2012-12-20 15:43:37 +0100

    add questionnaireController test, should fail on php 5.3

---
+++ tests/application/controllers/QuestionnaireControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+class Webenq_Test_ControllerTestCase_QuestionnaireControllerTest extends Webenq_Test_Case_Controller
+{
+    public function testLogin(){
+        $this->getRequest()->setMethod('POST')->setPost(
+            array(
+                'username' => 'admin',
+                'password' => 'webenq'
+            )
+        );
+        $this->dispatch('user/login');
+    }
+
+    public function testQuestionnaireViewIsRendered()
+    {
+        $this->dispatch('/questionnaire');
+        $this->assertController('questionnaire');
+        $this->assertQuery('tbody.questionnaire');
+    }
+    public function tearDown(){
+        $this->dispatch('user/logout');
+    }
+}
