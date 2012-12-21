38edad84574074a347a096d7f535a152363ed877
Jaap-Andre de Hoop <j.dehoop@nivocer.com>
2012-12-21 12:32:43 +0100

    add test for correct controller (and no parse errors) activate assertquery test, gives probably a error on travis, not local cleanup

--- tests/application/controllers/QuestionnaireControllerTest.php
+++ tests/application/controllers/QuestionnaireControllerTest.php
@@ -1,25 +1,17 @@
 <?php
 class Webenq_Test_ControllerTestCase_QuestionnaireControllerTest extends Webenq_Test_Case_Controller
 {
-//    public function testLogin(){
-//        $this->getRequest()->setMethod('POST')->setPost(
-//            array(
-//                'username' => 'admin',
-//                'password' => 'webenq'
-//            )
-//        );
-//        $this->dispatch('user/login');
-//    }
+    public function testCorrectControllerIsUsed()
+    {
+        $this->dispatch('/questionnaire');
+        $this->assertController('questionnaire');
+    }
 
     public function testQuestionnaireViewIsRendered()
     {
         $this->loadDatabase();
-//
-//        $this->dispatch('/questionnaire');
-//        $this->assertController('questionnaire');
-//        $this->assertQuery('tbody.questionnaire');
+        $this->dispatch('/questionnaire');
+        $this->assertQuery('tbody.questionnaire');
+
     }
-//    public function tearDown(){
-//        $this->dispatch('user/logout');
-//    }
 }
