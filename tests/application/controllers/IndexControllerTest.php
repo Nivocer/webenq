dc03aec25ef58bd671ea1cf84077a72314df1b83
Jaap-Andre de Hoop <j.dehoop@nivocer.com>
2012-12-21 12:31:31 +0100

    activate test for correct controller (and no parse errors)

--- tests/application/controllers/IndexControllerTest.php
+++ tests/application/controllers/IndexControllerTest.php
@@ -4,8 +4,13 @@ class Webenq_Test_ControllerTestCase_IndexControllerTest extends Webenq_Test_Cas
     public function testIndexActionRendersLoginForm()
     {
 // @todo check test, phpunit is not redirected to user/login,
-// last controller is questionnaire (which is correct, because 'questionnaire' is last controller on stack)
 //        $this->dispatch('/');
 //        $this->assertRedirectTo('/user/login');
+    }
+    public function testCorrectControllerIsUsed()
+    {
+        //last controller is questionnaire (which is correct, because 'questionnaire' is last controller on stack)
+        $this->dispatch('/');
+        $this->assertController('questionnaire');
     }
 }
